1d9381a42ee5f6d31b66be564b4127505c767bfb
Administrator <Administrator@RAM-SERVER>
2021-07-29 13:21:15 +0530

    Add project files.

---
+++ VFPCefSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VFPCefSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Piyush Vaishnavswami")]
+[assembly: AssemblyProduct("VFPCefSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b8f32bf-ba83-42fe-81ea-b3dc0ef82f2a")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VFPCefSharp/VFPCefSharpUserControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VFPCefSharp
+{
+
+    [Guid("09A74F29-AB1B-479B-B6C5-030E13396AA8")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("VFPCefSharpUserControl")]
+    [ComVisible(true)]
+    public partial class VFPCefSharpUserControl : UserControl
+    {
+        public CefSharp.WinForms.ChromiumWebBrowser chromiumWeb;
+        public VFPCefSharpUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadBrowser(string url)
+        {
+            this.SuspendLayout();
+            chromiumWeb = new CefSharp.WinForms.ChromiumWebBrowser(url);
+            chromiumWeb.Dock = DockStyle.Fill;
+            this.Controls.Add(chromiumWeb);
+            this.ResumeLayout();
+        }
+
+    }
+}
